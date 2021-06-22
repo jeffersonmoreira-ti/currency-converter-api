b64ff4bf2911a9bd31ae4b8e15b3a76ba22938f3
Jefferson Moreira <jeffersonmoreira.ti@gmail.com>
2021-06-22 01:50:09 -0300

    Add API Documentation (OpenAPI/Swagger)

--- src/main/java/com/api/currencyconverterapi/CurrencyConverterApiApplication.java
+++ src/main/java/com/api/currencyconverterapi/CurrencyConverterApiApplication.java
@@ -1,15 +1,17 @@
 package com.api.currencyconverterapi;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
+
 @SpringBootApplication
-@ComponentScan(basePackages={"com.api.currencyconverterapi"})
+@ComponentScan(basePackages = {"com.api.currencyconverterapi"})
 public class CurrencyConverterApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CurrencyConverterApiApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyConverterApiApplication.class, args);
+    }
 }
