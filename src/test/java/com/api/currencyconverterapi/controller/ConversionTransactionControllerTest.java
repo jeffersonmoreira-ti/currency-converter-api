1be2d78420b7ed3cb8e0dafb847885b75e51d145
Jefferson Moreira <jeffersonmoreira.ti@gmail.com>
2021-06-24 13:43:58 -0300

    Changes Http statuses

---
+++ src/test/java/com/api/currencyconverterapi/controller/ConversionTransactionControllerTest.java
@@ -0,0 +1,11 @@
+package com.api.currencyconverterapi.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class ConversionTransactionControllerTest {
+
+    @Test
+    void getConversionTransaction() {
+    }
+}
